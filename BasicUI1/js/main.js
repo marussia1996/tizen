9f0d9e6f11c13f2c33322e4109cfc5556c919a71
maria <student@student-RnD>
2017-07-04 11:13:11 +0300

    1

---
+++ BasicUI1/js/main.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+    // TODO:: Do your initialization job
+
+    // add eventListener for tizenhwkey
+    document.addEventListener('tizenhwkey', function(e) {
+        if (e.keyName === "back") {
+            try {
+                tizen.application.getCurrentApplication().exit();
+            } catch (ignore) {}
+        }
+    });
+
+    // Sample code
+   // var mainPage = document.querySelector('#main');
+
+   // mainPage.addEventListener("click", function() {
+    //    var contentText = document.querySelector('#content-text');
+
+    //    contentText.innerHTML = (contentText.innerHTML === "Basic") ? "Tizen" : "Basic";
+  //  });
+    
+    document.querySelector('#rasm').addEventListener("click", function(){
+    var contentText = document.querySelector('#content-text');
+    contentText.innerHTML = 'Разрешение экрана: <b>'+screen.width+'X'+ screen.height+'</b>';
+    contentText.style.display = (contentText.style.display === "block") ? "none" : "block";
+    });
+    
+    document.querySelector('#show').addEventListener("click", function(){
+    	var contentText = document.querySelector('.ima');
+    	contentText.style.display = "block";
+    });
+    document.querySelector('#hide').addEventListener("click", function(){
+    	var contentText = document.querySelector('.ima');
+    	contentText.style.display = "none";
+    });
+};
